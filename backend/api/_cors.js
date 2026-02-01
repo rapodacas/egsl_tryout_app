4a289350876c29dac24d63c2bca7226abc2c55b9
rapodacas <raoul.fresno@gmail.com>
2026-01-31 22:16:46 -0800

    Fixed Cors to allow Authorization Headers, X-Requested-With, Accept and Origin

--- backend/api/_cors.js
+++ backend/api/_cors.js
@@ -4,7 +4,10 @@ function withCors(handler) {
   return async (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Content-Type, Authorization, X-Requested-With, Accept, Origin"
+    );
 
     if (req.method === "OPTIONS") {
       return res.status(200).end();

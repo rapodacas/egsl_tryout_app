88dc309b8ccae209665c833b931b4966b9fecb8a
rapodacas <raoul.fresno@gmail.com>
2026-02-08 22:47:16 -0800

    Added cors shim for express like handling

--- backend/api/_cors.js
+++ backend/api/_cors.js
@@ -2,12 +2,28 @@
 
 function withCors(handler) {
   return async (req, res) => {
+    // CORS headers
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Content-Type, Authorization, X-Requested-With, Accept, Origin"
     );
+
+    // Express-like res helpers
+    res.status = function (code) {
+      res.statusCode = code;
+      return res;
+    };
+
+    res.json = function (obj) {
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(obj));
+    };
+
+    // Express-like req.query
+    const url = new URL(req.url, `http://${req.headers.host}`);
+    req.query = Object.fromEntries(url.searchParams.entries());
 
     if (req.method === "OPTIONS") {
       return res.status(200).end();

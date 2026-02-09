e00194938d58388ab1a3c6eb3e9c76c4884576c1
rapodacas <raoul.fresno@gmail.com>
2026-02-08 20:43:31 -0800

    fix(api): move unified router to api/route.js for Vercel compatibility

--- api/[...slug].js
+++
@@ -1,9 +0,0 @@
-// api/[...slug].js
-// Catch-all Vercel serverless entry that forwards /api/* to backend/api/route.js
-
-const router = require("../backend/api/route.js");
-
-// Export as a Vercel handler function
-module.exports = async (req, res) => {
-  return await router(req, res);
-};
